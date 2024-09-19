2580ed5df50853e7f42333a515fb280e666f5ade
Vamshi Baja <vamshibaja@gmail.com>
2024-09-19 14:16:02 +0530

    Logic is ready

---
+++ db/models/thread.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+ const threadSchema = new mongoose.Schema({
+    content:{type:String,required:true},
+    timestamp:{type:Date,default:Date.now},
+    likes:{type:Number,default:0},
+    user:{type:String,default:"Vamshi@Baja"}
+
+ })
+
+ const Thread = mongoose.models.Thread || mongoose.model('Thread',threadSchema);
+ export {Thread};
